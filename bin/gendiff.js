1bdc3ef9fba612241f4e57cb0d043a2ff8e12033
pushinka <dekimiq@gmail.com>
2023-02-15 13:58:42 +0300

    step 2 - add adrgumenrs

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('3.4.4')
+  .version('1.0')
   .option('-f, --format <type>', 'output format', 'stylish')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .parse(process.argv);
