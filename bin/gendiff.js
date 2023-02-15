a0754688360947e1429585f00bb909b08fb8cd9d
pushinka <dekimiq@gmail.com>
2023-02-15 11:31:41 +0300

    fix path for commander

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
-
 import { Command } from 'commander';
 
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1')
+  .version('3.4.4')
   .option('-f, --format <type>', 'output format', 'stylish')
-
-program.parse();
+  .parse(process.argv);
