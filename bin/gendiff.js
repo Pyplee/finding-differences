6627777a311c949843fa2f67d5967b60ea236bcf
pyplee <dekimiq@gmail.com>
2023-02-28 12:10:23 +0300

    add .arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ const program = new Command();
 
 program
 	.description('Compares two configuration files and shows a difference.')
-	.option('-v, --version', 'output the version number');
+	.arguments('<filepath1> <filepath2>')
+	.option('-v, --version', 'output the version number')
+	.option('-f, --format <type>', 'output format');
 
 program.parse(process.argv);
