215e2790aa69dcfe9247e562ee7de4b5cb76e2dc
pyplee <dekimiq@gmail.com>
2023-03-01 13:39:47 +0300

    add tabs

--- src/getArrDiff.js
+++ src/getArrDiff.js
@@ -4,14 +4,14 @@ const getArrDiff = (data1, data2) => {
 	const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
 	const result = keys.map((key) => {
 		if (Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
-			const acc = data1[key] === data2[key] ? [`  ${key}: ${data1[key]}`] : [`- ${key}: ${data1[key]}`, `+ ${key}: ${data2[key]}`];
+			const acc = data1[key] === data2[key] ? [`	  ${key}: ${data1[key]}`] : [`	- ${key}: ${data1[key]}`, `	+ ${key}: ${data2[key]}`];
 			return acc;
 		}
 		if (Object.hasOwn(data1, key) && !Object.hasOwn(data2, key)) {
-			return [`- ${key}: ${data1[key]}`];
+			return [`	- ${key}: ${data1[key]}`];
 		}
 		if (!Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
-			return [`+ ${key}: ${data2[key]}`];
+			return [`	+ ${key}: ${data2[key]}`];
 		}
 	});
 	return result.flat(1);
