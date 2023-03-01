c386f1dede4c23eb0f7e67df56bc2c91d1827f03
pyplee <dekimiq@gmail.com>
2023-03-01 13:40:26 +0300

    add output func

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 const program = new Command();
 
 program
 	.description('Compares two configuration files and shows a difference.')
-	.arguments('<filepath1> <filepath2>')
+	.arguments('<filepath1>', 'path to file1')
+	.arguments('<filepath1>', 'path to file2')
 	.option('-v, --version', 'output the version number')
-	.option('-f, --format <type>', 'output format');
+	.option('-f, --format <type>', 'output format')
+	.action((filepath1, filepath2) => {
+		console.log('{\n' + genDiff(filepath1, filepath2) + '\n}');
+	});
 
 program.parse(process.argv);
