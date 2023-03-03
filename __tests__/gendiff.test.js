c309af532f6be3cd6470c2a62c4c16394a49e59f
pyplee <dekimiq@gmail.com>
2023-03-03 16:56:15 +0300

    work test

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,16 @@
+import { expect, test } from '@jest/globals';
+import { readFileSync } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const getRes = () => readFileSync(getFixturePath('expected_file.txt'), 'utf8');
+const result = getRes();
+
+test('Test gendiff', () => {
+  expect((genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')))).toEqual(result);
+})
