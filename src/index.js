e963d4021f563c66a035322ee235fcf3f77f52fb
pyplee <dekimiq@gmail.com>
2023-03-03 14:00:35 +0300

    fix errors lint

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
 import { readFileSync } from 'fs';
-import getArrDiff from './getArrDiff.js';
 import { resolve } from 'path';
 import { cwd } from 'process';
+import getArrDiff from './getArrDiff.js';
 
 const getReadFile = (file) => readFileSync(file, 'utf8');
 
 const getParseFile = (file) => JSON.parse(file);
 
 const getPrepare = (file) => {
-	const readedFile = getReadFile(file);
-	return getParseFile(readedFile);
+  const readedFile = getReadFile(file);
+  return getParseFile(readedFile);
 };
 
 const getFullPathToFile = (filepath) => resolve(cwd(), filepath);
 
 const genDiff = (filepath1, filepath2) => {
+  const fullPath1 = getFullPathToFile(filepath1);
+  const fullPath2 = getFullPathToFile(filepath2);
 
-	const fullPath1 = getFullPathToFile(filepath1);
-	const fullPath2 = getFullPathToFile(filepath2);
+  const data1 = getPrepare(fullPath1);
+  const data2 = getPrepare(fullPath2);
 
-	const data1 = getPrepare(fullPath1);
-	const data2 = getPrepare(fullPath2);
+  const result = getArrDiff(data1, data2);
 
-	const result = getArrDiff(data1, data2);
-
-	return result.join('\n');
-}
+  return result.join('\n');
+};
 
 export default genDiff;
