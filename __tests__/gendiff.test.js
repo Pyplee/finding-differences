d4c0a8b79d6cf7909f5d870b534c20fad71c7c95
pyplee <dekimiq@gmail.com>
2023-03-06 15:03:46 +0300

    fix result

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,11 +10,10 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const getRead = (filename) => readFileSync(getFixturePath(filename), 'utf8');
 
-const resultJson = getRead('resultGenDiffJson.txt');
-const resultYaml = getRead('resultGenDiffYaml.txt');
+const result = getRead('resultGenDiff.txt');
 
 test('Test gendiff', () => {
-  expect((genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')))).toEqual(resultJson);
-  expect((genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml')))).toEqual(resultYaml);
-  expect((genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml')))).toEqual(resultYaml);
+  expect((genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')))).toEqual(result);
+  expect((genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml')))).toEqual(result);
+  expect((genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml')))).toEqual(result);
 });
