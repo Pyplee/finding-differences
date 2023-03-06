a783b32f87e319d17d163ba49fd86c9e3d37c95e
pyplee <dekimiq@gmail.com>
2023-03-06 15:00:29 +0300

    add test hard structure

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,12 +8,13 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getRead = (filename) => readFileSync(getFixturePath(filename), 'utf8');
 
-const getRes = () => readFileSync(getFixturePath('expected_file.txt'), 'utf8');
-const result = getRes();
+const resultJson = getRead('resultGenDiffJson.txt');
+const resultYaml = getRead('resultGenDiffYaml.txt');
 
 test('Test gendiff', () => {
-  expect((genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')))).toEqual(result);
-  expect((genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml')))).toEqual(result);
-  expect((genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml')))).toEqual(result);
+  expect((genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')))).toEqual(resultJson);
+  expect((genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml')))).toEqual(resultYaml);
+  expect((genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml')))).toEqual(resultYaml);
 });
