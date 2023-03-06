1d4ba2f530d4816895704b2f989b108b2455c394
pyplee <dekimiq@gmail.com>
2023-03-06 13:58:53 +0300

    add and write code parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,1 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { load } from 'js-yaml';
 
+const getParse = (file, extension) => {
+  if (extension === 'yaml' || extension === 'yml') {
+    return load(file);
+  }
+  if (extension === 'js') {
+    return JSON.parse(file);
+  }
+  return false;
+};
+
+export default getParse;
