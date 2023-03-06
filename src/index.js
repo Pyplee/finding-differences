6a9e77a72a559c9e30f59e11ebf8082bd7bb206d
pyplee <dekimiq@gmail.com>
2023-03-06 15:28:35 +0300

    fix save

--- src/index.js
+++ src/index.js
@@ -6,20 +6,17 @@ import getParse from './parsers.js';
 
 const getReadFile = (file) => readFileSync(file, 'utf8');
 const getExtension = (filepath) => extname(filepath);
-
-const getPrepare = (file, format) => {
-  const readedFile = getReadFile(file);
-  return getParse(readedFile, format);
-};
-
 const getFullPathToFile = (filepath) => resolve(cwd(), filepath);
 
 const genDiff = (filepath1, filepath2) => {
   const fullPath1 = getFullPathToFile(filepath1);
   const fullPath2 = getFullPathToFile(filepath2);
 
-  const tree1 = getPrepare(fullPath1, getExtension(filepath1));
-  const tree2 = getPrepare(fullPath2, getExtension(filepath2));
+  const readFile1 = getReadFile(fullPath1);
+  const readFile2 = getReadFile(fullPath2);
+
+  const tree1 = getParse(readFile1, getExtension(filepath1));
+  const tree2 = getParse(readFile2, getExtension(filepath2));
 
   const result = getDiffForGen(tree1, tree2);
 
