609ca109129f77c744a1fd5ac48d6c9645245155
pyplee <dekimiq@gmail.com>
2023-03-09 10:17:56 +0300

    rewrite code note getParentNode

--- src/buildTreeDiff.js
+++ src/buildTreeDiff.js
@@ -1,18 +1,27 @@
 import _ from 'lodash';
 
-const getDiffForGen = (data1, data2) => {
-  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
-  const result = keys.map((key) => {
-    if (Object.hasOwn(data1, key) && Object.hasOwn(data2, key)) {
-      const acc = data1[key] === data2[key] ? [`    ${key}: ${data1[key]}`] : [`  - ${key}: ${data1[key]}`, `  + ${key}: ${data2[key]}`];
-      return acc;
+const getLeaf = (key, type, value) => ({ key, type, value });
+const getParents = (key, type, children) => ({ key, type, children });
+const getChangedLeaf = (key, type, oldValue, newValues) => ({
+  key, type, oldValue, newValues,
+});
+
+const buildTreeDiff = (data1, data2) => {
+  const sortedKeys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
+
+  const result = sortedKeys.map((key) => {
+    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+      return getParents(key, 'uchanged', buildTreeDiff(data1[key], data2[key]));
     }
-    if (Object.hasOwn(data1, key) && !Object.hasOwn(data2, key)) {
-      return [`  - ${key}: ${data1[key]}`];
+    if (!_.has(data1)) {
+      return getLeaf(key, 'added', data2[key]);
     }
-    return [`  + ${key}: ${data2[key]}`];
+    if (!_.has(data2)) {
+      return getLeaf(key, 'removed', data1[key]);
+    }
+    return getChangedLeaf(key, 'unchanged', data1[key], data2[key]);
   });
-  return result.flat(1);
+  return result;
 };
 
-export default getDiffForGen;
+export default buildTreeDiff;
