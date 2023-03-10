bcd7b99358f21bfedf8866d6d8c6eb207fd8fe35
pyplee <dekimiq@gmail.com>
2023-03-10 18:07:58 +0300

    fix path

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const getRead = (filename) => readFileSync(getFixturePath(filename), 'utf8');
 
-const result = getRead('resultGenDiff.txt');
+const result = getRead('stylishOutput.txt');
 
 test('Test gendiff', () => {
   expect((genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')))).toEqual(result);
