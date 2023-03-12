04a67bc80ebd749291aaac3ff873e12e6a5273ca
pyplee <dekimiq@gmail.com>
2023-03-12 14:17:12 +0300

    write hub for formatters

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,1 +1,14 @@
+import buildStylish from './stylish.js';
+import buildPlain from './plain.js';
 
+const identifyStyleAndCall = (treeDiff, style) => {
+  if (style === 'stylish') {
+    return buildStylish(treeDiff);
+  }
+  if (style === 'plain') {
+    return buildPlain(treeDiff);
+  }
+  return `Style: ${style} is undefined, check spelling or print help - 'gendiff -h'`;
+};
+
+export default identifyStyleAndCall;
