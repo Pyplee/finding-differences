e9459b6b851bf35d53e084e098bfe5055fb93f9c
pyplee <dekimiq@gmail.com>
2023-03-12 14:15:01 +0300

    rewrite test for nested structure

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,10 +10,19 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const getRead = (filename) => readFileSync(getFixturePath(filename), 'utf8');
 
-const result = getRead('stylishOutput.txt');
+const cases = [
+  ['stylishOutput.txt', 'stylish', 'file1.json', 'file2.json'],
+  ['stylishOutput.txt', 'stylish', 'file1.yml', 'file2.yml'],
+  ['stylishOutput.txt', 'stylish', 'file1.yaml', 'file2.yaml'],
+  ['plainOutput.txt', 'plain', 'file1.json', 'file2.json'],
+  ['plainOutput.txt', 'plain', 'file1.json', 'file2.json'],
+  ['plainOutput.txt', 'plain', 'file1.json', 'file2.json'],
+];
 
-test('Test gendiff', () => {
-  expect((genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')))).toEqual(result);
-  expect((genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml')))).toEqual(result);
-  expect((genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml')))).toEqual(result);
+test.each(cases)('Test all formats and all styles', (output, style, file1, file2) => {
+  expect(genDiff(getFixturePath(file1), getFixturePath(file2), style)).toEqual(getRead(output));
+});
+
+test('Test error', () => {
+  expect((genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'AUF'))).toEqual('Style: AUF is undefined, check spelling or print help - \'gendiff -h\'');
 });
