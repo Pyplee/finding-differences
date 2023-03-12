ba15ce58a404497b93d33341de3100c4fdd3d513
pyplee <dekimiq@gmail.com>
2023-03-12 14:17:59 +0300

    write func plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,1 +1,44 @@
+import _ from 'lodash';
 
+const getValueFormat = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  if (typeof (value) === 'string') {
+    return `'${value}'`;
+  }
+  return `${value}`;
+};
+
+const buildPlain = (diff, fileName = []) => {
+  const {
+    name, type, value, children, oldValue, newValue,
+  } = diff;
+  const nestedKey = [...fileName, name];
+  const joinPath = nestedKey.join('.');
+  switch (type) {
+    case 'root': {
+      const result = children
+        .filter((child) => child.type !== 'unchanged')
+        .flatMap((child) => buildPlain(child, []));
+      return result.join('\n');
+    }
+    case 'nested': {
+      const result = children
+        .filter((child) => child.type !== 'unchanged')
+        .flatMap((child) => buildPlain(child, nestedKey));
+      return result.join('\n');
+    }
+    case 'added':
+      return `Property '${joinPath}' was added with value: ${getValueFormat(value)}`;
+    case 'removed':
+      return `Property '${joinPath}' was removed`;
+    case 'changed': {
+      return `Property '${joinPath}' was updated. From ${getValueFormat(oldValue)} to ${getValueFormat(newValue)}`;
+    }
+    default:
+      return `Type: ${type} is undefined`;
+  }
+};
+
+export default buildPlain;
